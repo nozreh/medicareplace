b9d9a5648f42ffebc1754baa9df081d2c63f24cf
Robert <rothattack@gmail.com>
2013-06-10 11:04:10 -0500

    still editing @rothattack

--- addons/shared_addons/modules/plan/views/partials/filters.php
+++ addons/shared_addons/modules/plan/views/partials/filters.php
@@ -6,13 +6,13 @@
         <?php echo form_hidden('f_module', $module_details['slug']); ?>
 		<ul>  
 			<li>
-        		Companies
+        		Companies<br />
                         <?php foreach($company_list as $id => $company): ?>
                             <input type="checkbox" name="f_company[]" id="f_company" checked="checked" value="<?php echo $company->id; ?>" style="margin:10px" /><?php echo $company->name; ?></br>
                         <?php endforeach; ?>
                         </li>
                         <li>
-        		Plans
+        		Plans<br />
                         <?php foreach($plan_list as $plan): ?>
                             <input type="checkbox" name="f_plan[]" id="f_plan" value="<?php echo $plan->id; ?>" text="<?php echo $plan->name; ?>" checked="checked" style="margin:10px" /><?php echo $plan->name; ?></br>
                         <?php endforeach; ?>
--- addons/shared_addons/themes/GITS/views/modules/plan/select_plan.php
+++ addons/shared_addons/themes/GITS/views/modules/plan/select_plan.php
@@ -1,37 +1,25 @@
-<div  class="row span3">
+<div  class="row span4">
     <h2 class="page-title" id="page_title"><?php echo 'User Info'; ?></h2>
     <p>
         <span><?php echo 'Step 1'; ?></span>-&gt;
         <span><?php echo 'Step 2'; ?></span>-&gt;
         <span id="active_step"><?php echo 'Step 3'; ?></span>
     </p>
-
-    <section class="title">
-        <h4>Rates</h4>
-    </section>
-
+    
     <section class="item">
-
         <?php if ($rates) : ?>
-
             <?php echo $this->load->view('partials/filters'); ?>
-
             <div id="filter-stage">
-
                 <?php echo form_open('plan/select_plan'); ?>
-
+                <?php echo '<h4>Rates</h4>'; ?>
                 <?php echo $this->load->view('tables/plans'); ?>
-
                 <?php echo form_close(); ?>
-
             </div>
-
         <?php else : ?>
             <div class="no_data">No Available Plan</div>
         <?php endif; ?>
-
     </section>
-    
+
     <div id="names_div" class="well-large">
         <!-- BEGIN APPLICANT -->
         <div class="dependants">
