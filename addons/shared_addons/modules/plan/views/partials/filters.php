5ef03eb977098aab689c3a937363f250d10397a1
Robert <rothattack@gmail.com>
2013-06-10 13:45:45 -0500

    that's it for today

--- addons/shared_addons/modules/plan/views/partials/filters.php
+++ addons/shared_addons/modules/plan/views/partials/filters.php
@@ -6,15 +6,15 @@
             <?php foreach ($company_list as $id => $company): ?>
                 <input type="checkbox" name="f_company[]" id="f_company" checked="checked" value="<?php echo $company->id; ?>" style="margin:10px" /><?php echo $company->name; ?></br>
             <?php endforeach; ?>
-                <button class="btn btn-small btn-primary">CLEAR ALL</button>
+                <button id="clear-all" class="btn btn-small btn-primary">CLEAR ALL</button>
         </div>
         <div class="white-panel plan-selection">
             <h5>PLAN SELECTION:</h5>
             <?php foreach ($plan_list as $plan): ?>
                 <input type="checkbox" name="f_plan[]" id="f_plan" value="<?php echo $plan->id; ?>" text="<?php echo $plan->name; ?>" checked="checked" style="margin:10px" /><?php echo $plan->name; ?></br>
             <?php endforeach; ?>
-                <button class="btn btn-small btn-primary">CLEAR ALL</button>
-                <button class="btn btn-small btn-success">PLAN F ONLY</button>
+                <button id="clear-all2" class="btn btn-small btn-primary">CLEAR ALL</button>
+                <button id="plan-f-only" class="btn btn-small btn-success">PLAN F ONLY</button>
         </div>
     <?php echo form_close(); ?>
 </fieldset>
